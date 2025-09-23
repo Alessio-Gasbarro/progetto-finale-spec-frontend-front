26a7280ef55040eb2058846e522fe1c949c5f19a
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-09-23 11:03:13 +0200

    [UPD] Added base Project Vite

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Strumenti Musicali</h1>
+      <p>Benvenuto! Qui mostreremo la lista degli strumenti nei prossimi step.</p>
+    </div>
+  );
+}
